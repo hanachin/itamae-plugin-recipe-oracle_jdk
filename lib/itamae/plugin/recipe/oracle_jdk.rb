1849fc29b70006aa377bf8541b80e0a4426254dd
Seiei Higa <hanachin@gmail.com>
2014-12-27 12:18:00 +0900

    install oracle sdk

--- lib/itamae/plugin/recipe/oracle_jdk.rb
+++ lib/itamae/plugin/recipe/oracle_jdk.rb
@@ -20,10 +20,16 @@ directory jdk_dir do
   action :create
 end
 
-current_jdk_ver = "jdk1.8.0_25"
+current_jdk_ver   = "jdk1.8.0_25"
+jdk_installed_dir = "#{jdk_dir}/#{current_jdk_ver}"
 
 execute 'extract oracle jdk' do
   command "tar zxvf #{filename} -C #{jdk_dir}"
   cwd     work_dir
-  not_if  "test -d #{jdk_dir}/#{current_jdk_ver}"
+  not_if  "test -d #{jdk_installed_dir}"
+end
+
+execute 'install oracke jdk' do
+  command "update-alternatives --install /usr/bin/java java #{jdk_installed_dir}/bin/java 100"
+  not_if "update-alternatives --display java | grep #{jdk_installed_dir}"
 end
